766e2ecfa17d7625f1217e517bc22a6d4dc54d3d
Taisei-Shinoda <aaatshinodaaat@gmail.com>
2022-08-20 21:35:47 +0900

    Initial Commit

---
+++ swiftui-chat/swiftui_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chatApp.swift
+//  swiftui-chat
+//
+//  Created by Taisei Shinoda on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
